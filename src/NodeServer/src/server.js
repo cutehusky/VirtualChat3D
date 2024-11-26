5dc7a2e498d00f57cae058155f08447366e2649f
dtnolife283 <tdtuan22@clc.fitus.edu.vn>
2024-11-26 09:52:17 +0700

    Create event for friend process

--- src/NodeServer/src/server.js
+++ src/NodeServer/src/server.js
@@ -1,8 +1,14 @@
 NetworkController = require("./NetworkController.js")
 fb = require("./FirebaseDataModel.js")
 message = require("./MessageController.js")
+friend = require("./FriendController.js")
 
 network = NetworkController.getInstance()
 network.SubscribeEvent('sendMessage', message.processSendMessage);
 network.SubscribeEvent('viewMessage', message.processViewMessage);
 network.SubscribeEvent('readMessage', message.processReadMessage);
+network.SubscribeEvent('sendFriendRequest', friend.processFriendRequest);
+network.SubscribeEvent('friendRequestAccept', friend.processFriendRequestAccept);
+network.SubscribeEvent('friendRequestRefuse', friend.processFriendRequestRefuse);
+network.SubscribeEvent('processViewFriendList', friend.processViewFriendList);
+network.SubscribeEvent('processRemoveFriend', friend.processRemoveFriend);
