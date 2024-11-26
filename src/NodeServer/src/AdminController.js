19936425a144b090d66533ab98eae55902161e17
dtnolife283 <tdtuan22@clc.fitus.edu.vn>
2024-11-26 14:08:55 +0700

    admin functional

--- src/NodeServer/src/AdminController.js
+++ src/NodeServer/src/AdminController.js
@@ -1,14 +1,53 @@
+NetworkController = require("./NetworkController.js")
+Firebase = require("./FirebaseDataModel.js")
+
+/*
+Data format:
+{
+    uid: string
+}
+*/
+
 class AdminController {
     static processLockUser(socket, data) {
-
+        network = NetworkController.getInstance();
+        if (data.uid in network.clientList) {
+            network.clientList[data.uid].emit('lockUser', data);
+            for (let key in network.clientList) {
+                if (key !== data.uid)
+                    network.clientList[key].emit('hideLockUser', data);
+            }
+        }
+        let fb = Firebase.getInstance();
+        fb.lockUser(data.uid);
     }
     static processUnlockUser(socket, data) {
-
+        network = NetworkController.getInstance();
+        if (data.uid in network.clientList) {
+            network.clientList[data.uid].emit('unlockUser', data);
+            for (let key in network.clientList) {
+                if (key !== data.uid)
+                    network.clientList[key].emit('unhideUnlockUser', data);
+            }
+        }
+        let fb = Firebase.getInstance();
+        fb.unlockUser(data.uid);
     }
     static processRemoveUser(socket, data) {
-
+        network = NetworkController.getInstance();
+        if (data.uid in network.clientList) {
+            network.clientList[data.uid].emit('removeUser', data);
+            for (let key in network.clientList) {
+                if (key !== data.uid)
+                    network.clientList[key].emit('hideRemoveUser', data);
+            }
+        }
+        let fb = Firebase.getInstance();
+        fb.deleteUser(data.uid);
     }
     static processViewSystemInfo(socket, data) {
         
     }
 }
+
+module.exports = AdminController
