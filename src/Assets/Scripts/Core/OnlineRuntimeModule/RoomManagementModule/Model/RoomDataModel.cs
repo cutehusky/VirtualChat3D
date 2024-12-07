37aaa6b44a5e3fc9b21dc7552a5ed1ba8b8e58a7
dtnolife283 <tdtuan22@clc.fitus.edu.vn>
2024-12-07 20:32:59 +0700

    Fetch Room List for host

--- src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/Model/RoomDataModel.cs
+++ src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/Model/RoomDataModel.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections.Generic;
 using Core.MVC;
 using Core.UserAccountModule.Model;
+using Firebase;
+using Firebase.Database;
 
 namespace Core.OnlineRuntimeModule.RoomManagementModule.Model
 {
@@ -13,6 +15,29 @@ namespace Core.OnlineRuntimeModule.RoomManagementModule.Model
         protected override void OnInit()
         {
             
+        }  
+
+        public void FetchRoomsList()
+        {
+            FirebaseDatabase.DefaultInstance.GetReference($"Account/{UserProfileDataModel.UserProfileData.UserId}/Rooms")
+                .GetValueAsync().ContinueWithOnMainThread(task =>
+                {
+                    if (task.IsFaulted)
+                    {
+                        return;
+                    }
+                    else if (task.IsCompleted)
+                    {
+                        DataSnapshot snapshot = task.Result;
+                        foreach (DataSnapshot roomSnapshot in snapshot.Children)
+                        {
+                            RoomData roomData = new RoomData();
+                            roomData.RoomId = roomSnapshot.Key;
+                            roomData.AccessType = (EAccessType)roomSnapshot.Child("AccessType").GetValue(false);
+                            RoomsData.Add(roomData);
+                        }
+                    }
+                });
         }
     }
 }
