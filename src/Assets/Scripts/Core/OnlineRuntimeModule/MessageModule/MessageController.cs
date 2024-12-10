3d698996dbd036cf490a2bb2f4af9f5036e6d5a6
JoeCao <joecao0303@gmail.com>
2024-12-10 12:50:05 +0700

    Controller of message in chat room

--- src/Assets/Scripts/Core/OnlineRuntimeModule/MessageModule/MessageController.cs
+++ src/Assets/Scripts/Core/OnlineRuntimeModule/MessageModule/MessageController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using Core.MessageModule.Model;
 using Core.MVC;
+using Core.UserAccountModule.Model;
 using QFramework;
 using Unity.Netcode;
 
@@ -8,21 +10,39 @@ namespace Core.OnlineRuntimeModule.MessageModule.Controller
     public class MessageController: NetworkBehaviour, IController
     {
         private MessageView _messageView;
+        private ChatSession _chatSession;
+
         public void OnInit(MessageView view)
         {
-            
+            _messageView = view;
+
+            _messageView.send.onClick.AddListener(() =>
+            {
+                SendMessageServerRpc(this.GetModel<UserProfileDataModel>().UserProfileData.UserId, _messageView.chatInput.Text);
+            });
+        }
+
+        public void OpenMessageView()
+        {
+            _messageView.Display();
+            _messageView.Render(_chatSession, this.GetModel<UserProfileDataModel>().UserProfileData.UserId);
         }
 
         [ServerRpc(RequireOwnership = false)]
-        public void SendMessageServerRpc(string username, string message)
+        public void SendMessageServerRpc(string userID, string message)
         {
-            
+            SendMessageClientRpc(userID, message);
         }
 
         [ClientRpc]
-        public void SendMessageClientRpc(string username, string message)
+        public void SendMessageClientRpc(string userID, string message)
         {
-            
+            _chatSession.ChatData.Add(new ChatMessage()
+            {
+                Content = message,
+                UserId = userID
+            });
+            _messageView.RefreshList();
         }
         
         public IArchitecture GetArchitecture()
