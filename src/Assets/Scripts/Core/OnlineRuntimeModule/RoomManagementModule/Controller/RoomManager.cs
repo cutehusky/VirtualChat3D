1be8cb8b60a9f6caebf9b67bb7cbe3b59432853d
JoeCao <joecao0303@gmail.com>
2024-12-10 15:47:19 +0700

    Host room view

--- src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/Controller/RoomManager.cs
+++ src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/Controller/RoomManager.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections.Generic;
 using Core.MVC;
+using Core.OnlineRuntimeModule.RoomManagementModule.Model;
 using Core.OnlineRuntimeModule.RoomManagementModule.View;
 using Core.UserAccountModule.Model;
 using Firebase;
 using Firebase.Database;
+using QFramework;
 
 namespace Core.OnlineRuntimeModule.RoomManagementModule.Controller
 {
-    public class RoomManager: ControllerBase
+    public class RoomManager : ControllerBase
     {
         private HostRoomView _hostRoomView;
         private JoinRoomView _joinRoomView;
@@ -27,32 +29,43 @@ namespace Core.OnlineRuntimeModule.RoomManagementModule.Controller
             _joinRoomView.Render(null);
             return _joinRoomView;
         }
-        
+
         public void CreateRoom()
         {
-            
+
         }
 
         public void EditRoomAccessType(string roomId)
         {
-            
+
+        }
+
+        public void HostRoom(RoomData roomData)
+        {
+            this.GetModel<RoomDataModel>().CurrentHostRoomData = roomData;
         }
 
         public void DeleteRoom(string roomId)
         {
-            
+            this.GetModel<RoomDataModel>().DeleteRoom(this.GetModel<UserProfileDataModel>().UserProfileData.UserId, roomId);
         }
 
         public void LoadRoomsData()
         {
-            
+            this.GetModel<RoomDataModel>().FetchRoomsList(this.GetModel<UserProfileDataModel>().UserProfileData.UserId);
         }
-        
+
         public override void OnInit(List<ViewBase> view)
         {
             base.OnInit(view);
             _hostRoomView = view[0] as HostRoomView;
             _joinRoomView = view[1] as JoinRoomView;
+            _hostRoomView.OnHostRoom += HostRoom;
+            _hostRoomView.OnDeleteRoom += DeleteRoom;
+            _hostRoomView.OnEditRoom += (roomId) =>
+            {
+
+            };
         }
     }
 }
--- src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/View/HostRoomView.cs
+++ src/Assets/Scripts/Core/OnlineRuntimeModule/RoomManagementModule/View/HostRoomView.cs
@@ -1,28 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using Core.MessageModule.Model;
+using Core.MessageModule.View;
 using Core.MVC;
+using Core.OnlineRuntimeModule.RoomManagementModule.Model;
 using TMPro;
+using UMI;
 using UnityEngine;
 using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 namespace Core.OnlineRuntimeModule.RoomManagementModule.View
 {
-    public class HostRoomView: ViewBase
+    public class HostRoomView : ViewBase, LoopScrollPrefabSource, LoopScrollDataSource
     {
         public TMP_InputField ip;
         public TMP_InputField port;
         public Button host;
-        
+        public RoomDataModel RoomDataModel;
+
         public override void Render(ModelBase model)
         {
+            RoomDataModel = model as RoomDataModel;
             ip.text = "localhost";
             port.text = "8888";
+            list.prefabSource = this;
+            list.dataSource = this;
+            list.totalCount = RoomDataModel.RoomsData.Count;
+            list.RefillCells();
         }
 
         public override void OnInit()
         {
-            
+
+        }
+        public LoopScrollRect list;
+
+        public void RefreshList()
+        {
+            list.totalCount = RoomDataModel.RoomsData.Count;
+            list.RefillCells();
+        }
+
+        public GameObject itemPrefab;
+        Stack<Transform> pool = new Stack<Transform>();
+        public GameObject GetObject(int index)
+        {
+            if (pool.Count == 0)
+            {
+                return Instantiate(itemPrefab);
+            }
+            var candidate = pool.Pop();
+            candidate.gameObject.SetActive(true);
+            return candidate.gameObject;
+        }
+
+        public void ReturnObject(Transform trans)
+        {
+            var item = trans.GetComponent<RoomListItem>();
+            item.delete.onClick.RemoveAllListeners();
+            item.host.onClick.RemoveAllListeners();
+            item.editEnvironment.onClick.RemoveAllListeners();
+            trans.gameObject.SetActive(false);
+            trans.SetParent(transform, false);
+            pool.Push(trans);
+        }
+
+        public Action<string> OnDeleteRoom;
+        public Action<string> OnEditRoom;
+        public Action<RoomData> OnHostRoom;
+        public void ProvideData(Transform trans, int index)
+        {
+            var item = trans.GetComponent<RoomListItem>();
+            if (index < RoomDataModel.RoomsData.Count)
+            {
+                item.delete.onClick.RemoveAllListeners();
+                item.host.onClick.RemoveAllListeners();
+                item.editEnvironment.onClick.RemoveAllListeners();
+                item.roomId.text = RoomDataModel.RoomsData.ElementAt(index).RoomId;
+                item.delete.onClick.AddListener(() =>
+                {
+                    OnDeleteRoom(RoomDataModel.RoomsData.ElementAt(index).RoomId);
+                });
+                item.host.onClick.AddListener(() =>
+                {
+                    OnHostRoom(RoomDataModel.RoomsData.ElementAt(index).RoomId);
+                });
+                item.editEnvironment.onClick.AddListener(() =>
+                {
+                    OnEditRoom(RoomDataModel.RoomsData.ElementAt(index).RoomId);
+                });
+            }
         }
     }
 }
