46f470e704ce7e818cd71050d89b3406057dae0a
JoeCao <joecao0303@gmail.com>
2024-12-20 23:50:00 +0700

    add Animation Controller

---
+++ src/Assets/Scripts/Core/OnlineRuntimeModule/CharacterControl/PlayerAnimationController .cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Core.OnlineRuntimeModule.InputModule.Model;
+using QFramework;
+using Unity.Netcode;
+
+namespace Core.OnlineRuntimeModule.CharacterControl
+{
+    public class PlayerAnimationController : NetworkBehaviour, IController
+    {
+        private PlayerController _playerController;
+        public void Start()
+        {
+            if (IsOwner)
+            {
+                _playerController = GetComponent<PlayerController>();
+                InputRegister();
+            }
+        }
+
+        public void InputRegister()
+        {
+            this.GetModel<PlayerInputAction>().GetTrigger("Applause").Register(() =>
+            {
+                if (_playerController && _playerController.animator)
+                    _playerController.animator.SetTrigger("Applause");
+            });
+            this.GetModel<PlayerInputAction>().GetTrigger("Waves").Register(() =>
+            {
+                if (_playerController && _playerController.animator)
+                    _playerController.animator.SetTrigger("Waves");
+            });
+            this.GetModel<PlayerInputAction>().GetTrigger("Crying").Register(() =>
+            {
+                if (_playerController && _playerController.animator)
+                    _playerController.animator.SetTrigger("Crying");
+            });
+            this.GetModel<PlayerInputAction>().GetTrigger("Laughing").Register(() =>
+            {
+                if (_playerController && _playerController.animator)
+                    _playerController.animator.SetTrigger("Laughing");
+            });
+        }
+
+        public IArchitecture GetArchitecture()
+        {
+            return CoreArchitecture.Interface;
+        }
+    }
+}
